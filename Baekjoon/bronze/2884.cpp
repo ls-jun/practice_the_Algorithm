fa6aed6c0896291ded07687e8ec5116db1b292b9
unknown <tndwns19@gmail.com>
2021-11-13 00:14:28 +0900

    bornze 3 알람시계

---
+++ Baekjoon/bronze/2884.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int hour, minute;
+    cin >> hour>>minute;
+    minute-=45;
+    if(minute<0){
+        minute+=60;
+        hour-=1;
+        if(hour<0) hour=23;
+    }
+    cout<<hour<<" "<<minute;
+}
