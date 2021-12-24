cd7e1995b463687cfb3964f6c5820640b41bda90
unknown <tndwns19@gmail.com>
2021-12-24 10:13:10 +0900

    silver4 요세푸스 문제0

---
+++ Baekjoon/silver/11866.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n, k;
+    cin >>n>> k;
+    int arr[n];
+    vector<int> answer;
+
+    for(int i=1;i<n;i++)
+        arr[i] = i;
+    arr[0] = n;
+
+    int point=1;
+    int check=0;
+    
+    while(answer.size()<n){
+   
+        if(arr[point%n] != 0)
+            check++;
+        
+        if(check==k){
+        answer.push_back(arr[point%n]);
+        arr[point%n] = 0;
+        check=0;
+        }
+        
+        point++;
+        
+    }
+    cout<<"<";
+    for(int i=0;i<answer.size()-1;i++)
+        cout<<answer[i]<<", ";
+    cout<<answer.back()<<">";
+
+    
+
+    
+
+}
