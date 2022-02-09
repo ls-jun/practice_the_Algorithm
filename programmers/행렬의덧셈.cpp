16bb0fb4801693a6937765e8f1de8e5275f5b5b0
ls-jun <tndwns19@gmail.com>
2022-02-09 12:07:00 +0900

    Level 1

---
+++ programmers/행렬의덧셈.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+    vector<vector<int>> answer;
+    int size1 = arr1.size();
+    int size2 = arr1[0].size();
+    for (int i =0; i<size1;i++){
+        for(int j=0; j<size2;j++){
+            arr1[i][j] +=arr2[i][j];
+        }
+    }
+    return arr1;
+}
