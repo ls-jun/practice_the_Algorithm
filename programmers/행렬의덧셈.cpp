75576730cd765268c8a3fc53c5f7cd2f227e6e07
ls-jun <tndwns19@gmail.com>
2022-03-05 19:14:27 +0900

    programmmers

--- programmers/행렬의덧셈.cpp
+++ programmers/행렬의덧셈.cpp
@@ -4,8 +4,8 @@
 
 using namespace std;
 
-vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
-    vector<vector<int>> answer;
+vector<vector<int> > solution(vector<vector<int> > arr1, vector<vector<int> > arr2) {
+    vector<vector<int> > answer;
     int size1 = arr1.size();
     int size2 = arr1[0].size();
     for (int i =0; i<size1;i++){
